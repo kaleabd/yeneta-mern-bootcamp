acb174d3ffcf4fb1049e3c8be300734819be4141
kaleabd <kaleabdereje94@gmail.com>
2023-07-12 12:25:37 +0300

    fixed the routes based on the second react class, and learned about navigate component

--- materio/src/App.jsx
+++ materio/src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, {useState} from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
 
 const App = () => {
+  const [isLogged, setIsLogged] = useState(true);
+
   return (
     <div className="main-container">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={isLogged ? <Home /> : <Navigate to='/login' replace={true}/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>
---
+++ materio/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center w-full'>
+        <h1>this is the homepage</h1>
+    </div>
+  )
+}
+
+export default Home
