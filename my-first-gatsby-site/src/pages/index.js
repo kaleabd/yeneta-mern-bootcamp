c3d9d9c83d4a84fd3a3f8f3d5899f060273c83d6
kaleabd <kaleabdereje94@gmail.com>
2023-08-01 17:26:04 +0300

    removing the submodules

---
+++ my-first-gatsby-site/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='flex justify-center text-4xl font-bold h-screen items-center uppercase'>
+      <h1>this is my first gatsbyjs website</h1>
+    </div>
+  )
+}
+
+export default index
